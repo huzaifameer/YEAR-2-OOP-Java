0a22570e592a4deea02f67963d9016cc2f194f83
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-11-27 09:38:11 +0530

    2

--- src/com/W1956114/SubClasses/Clothing.java
+++ src/com/W1956114/SubClasses/Clothing.java
@@ -1,4 +1,30 @@
 package com.W1956114.SubClasses;
 
 public class Clothing {
+    private String clothSize;
+    private String clothColor;
+
+    public Clothing() {
+    }
+
+    public Clothing(String clothSize, String clothColor) {
+        this.clothSize = clothSize;
+        this.clothColor = clothColor;
+    }
+
+    public String getClothSize() {
+        return clothSize;
+    }
+
+    public void setClothSize(String clothSize) {
+        this.clothSize = clothSize;
+    }
+
+    public String getClothColor() {
+        return clothColor;
+    }
+
+    public void setClothColor(String clothColor) {
+        this.clothColor = clothColor;
+    }
 }
