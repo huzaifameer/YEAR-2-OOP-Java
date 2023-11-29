fab7a02af3c12501ffcf96cdc9e985f0582b0d4b
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-11-29 21:37:01 +0530

    3

--- src/com/W1956114/Manager/WestminsterShoppingManager.java
+++ src/com/W1956114/Manager/WestminsterShoppingManager.java
@@ -1,5 +1,65 @@
 package com.W1956114.Manager;
 
-public class WestminsterShoppingManager {
+import com.W1956114.Super.Product;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class WestminsterShoppingManager implements ShoppingManager{
+    private final ArrayList<Product> productsMainList = new ArrayList<>();
+    @Override
+    public void addAProduct(Product product) {
+        Scanner input=new Scanner(System.in);
+        while (true){
+
+        }
+        /*if (productsMainList.size() < 50) {
+            productsMainList.add(product);
+            System.out.println("Product added successfully.");
+        } else {
+            System.out.println("Cannot add more products. Maximum limit reached.");
+        }*/
+    }
+
+    @Override
+    public void deleteAProduct(String productId) {
+        for (Product product : productsMainList) {
+            if (product.getProductID().equals(productId)) {
+                productsMainList.remove(product);
+                System.out.println("Product deleted successfully.");
+                return;
+            }
+        }
+        System.out.println("Product with ID " + productId + " not found.");
+    }
+
+    @Override
+    public void displayAProducts() {
+
+        for (Product product : productsMainList) {
+            product.displayInfo();
+            System.out.println("------------------------------");
+        }
+    }
+
+    @Override
+    public void saveToAFile(String fileName) {
+        /*try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            outputStream.writeObject(productList);
+            System.out.println("Products saved to file successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @Override
+    public void readFromAFile(String fileName) {
+        /*try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName))) {
+            productList = (List<Product>) inputStream.readObject();
+            System.out.println("Products read from file successfully.");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }*/
+    }
 }
--- src/com/W1956114/User/ShoppingCart.java
+++ src/com/W1956114/User/ShoppingCart.java
@@ -5,17 +5,17 @@ import com.W1956114.Super.Product;
 import java.util.ArrayList;
 
 public class ShoppingCart {
-    private final ArrayList<Product> productsList = new ArrayList<>();
+    private final ArrayList<Product> productsCartList = new ArrayList<>();
 
     public void addAProduct(Product product) {
-        productsList.add(product);
+        productsCartList.add(product);
         System.out.println(product.getProductName() + " added to the cart.");
     }
     /*this part should be edited later*/
     public void removeAProduct(String productID) {/*here the id should be an input value this is temp.*/
-        for (Product product : productsList) {
+        for (Product product : productsCartList) {
             if (product.getProductName().equals(productID)) {
-                productsList.remove(product);
+                productsCartList.remove(product);
                 System.out.println(productID + " removed from the cart.");
                 return;
             }
@@ -25,7 +25,7 @@ public class ShoppingCart {
 
     public double calculateTheTotal() {
         double totalCost = 0;
-        for (Product product : productsList) {
+        for (Product product : productsCartList) {
             totalCost += product.getProductPrice();
         }
         return totalCost;
