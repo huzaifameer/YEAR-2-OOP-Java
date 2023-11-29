bdde61e1c9c38b2801248bbb374e23ebaa772fd5
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-11-29 17:34:49 +0530

    3

--- src/com/W1956114/SubClasses/Clothing.java
+++ src/com/W1956114/SubClasses/Clothing.java
@@ -1,13 +1,16 @@
 package com.W1956114.SubClasses;
 
-public class Clothing{
+import com.W1956114.Super.Product;
+
+public class Clothing extends Product {
     private String clothSize;
     private String clothColor;
 
     public Clothing() {
     }
 
-    public Clothing(String clothSize, String clothColor) {
+    public Clothing(String productID, String productName, int availableQuantity, double productPrice, String clothSize, String clothColor) {
+        super(productID,productName,availableQuantity,productPrice);
         this.clothSize = clothSize;
         this.clothColor = clothColor;
     }
@@ -27,4 +30,14 @@ public class Clothing{
     public void setClothColor(String clothColor) {
         this.clothColor = clothColor;
     }
+    @Override
+    public void displayInfo() {
+        System.out.println("Clothing - " + getProductName());
+        System.out.println("ID: " + getProductID());
+        System.out.println("Price: $" + getProductPrice());
+        System.out.println("Size: " + getClothSize());
+        System.out.println("Color: " + getClothColor());
+        System.out.println("Available Items: " + getAvailableQuantity());
+    }
+    /*An extra method for no reason*/
 }
