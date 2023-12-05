f9a3561624564d50ba433f4a627998e169f2089e
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-12-05 08:01:14 +0530

    GUI-1

--- src/com/W1956114/User/ShoppingCart.java
+++ src/com/W1956114/User/ShoppingCart.java
@@ -2,10 +2,103 @@ package com.W1956114.User;
 
 import com.W1956114.Super.Product;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class ShoppingCart {
+public class ShoppingCart extends JFrame {
+    /*------------------------------------------------------------*/
+    private static JLabel labelTopLeft;
+    private static JComboBox<String> productTypeDropdown ;
+    private static JButton shoppingCartButton;
+    private static JTable productDataTable;
+    /*------------------------------------------------------------*/
     private final ArrayList<Product> productsCartList = new ArrayList<>();
+    public ShoppingCart(){
+        JFrame shoppingCart=new JFrame();
+        shoppingCart.setSize(500,600);
+
+        shoppingCart.setTitle("Westminster Shopping Center");
+        //JPanel panelTop=new JPanel(new GridLayout(1,4));
+        //---creating the left top label with the text
+        JPanel panelTop = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 20));
+        labelTopLeft = new JLabel("Select Product Category  ");
+        panelTop.add(labelTopLeft);
+
+        //Setting up the drop down menu with 3 items
+        String[] productTypes = {"Clothing", "Electronic", "All"};
+
+        productTypeDropdown = new JComboBox<>(productTypes);
+        productTypeDropdown.setPreferredSize(new Dimension(100,30));
+        productTypeDropdown.setSelectedIndex(2);
+        productTypeDropdown.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Get the selected product type from the dropdown
+                String selectedProductType = (String) productTypeDropdown.getSelectedItem();
+                // Perform actions based on the selected product type
+                System.out.println("Selected Product Type: " + selectedProductType);
+            }
+        });
+        panelTop.add(productTypeDropdown);
+
+        shoppingCartButton=new JButton("Shopping Cart");
+        JLabel gap=new JLabel();
+        panelTop.add(gap);
+        shoppingCartButton.setSize(200,40);
+        panelTop.add(shoppingCartButton);
+
+        shoppingCart.add("North",panelTop);
+        //---------------------------------------------//
+        // Center panel with JTable
+        JPanel panelCenter = new JPanel(new BorderLayout());
+
+        int horizontalPadding = 20;
+        panelCenter.setBorder(BorderFactory.createEmptyBorder(0, horizontalPadding, 0, horizontalPadding));
+
+        // Creating a sample table model with 5 columns
+        DefaultTableModel productModel = new DefaultTableModel();
+
+        productModel.addColumn("Product ID");
+        productModel.addColumn("Name");
+        productModel.addColumn("Category");
+        productModel.addColumn("Price ($)");
+        productModel.addColumn("Info");
+
+        // Adding some sample data
+        for (int i = 0; i < 43; i++) {
+            productModel.addRow(new Object[]{"Data " + (i + 1), "Data " + (i + 1), "Data " + (i + 1), "Data " + (i + 1), "Data " + (i + 1)});
+        }
+
+        productDataTable = new JTable(productModel);
+        productDataTable.setSize(300,300);
+        //panelCenter.add(new JScrollPane(productDataTable), BorderLayout.CENTER);
+        JTableHeader header = productDataTable.getTableHeader();
+        header.setFont(new Font(header.getFont().getName(), Font.BOLD, header.getFont().getSize()));
+        //panelCenter.add(Box.createRigidArea(new Dimension(20, 0)));
+        //panelCenter.add(new JSeparator(SwingConstants.HORIZONTAL));
+        JScrollPane scrollPane = new JScrollPane(productDataTable);
+        scrollPane.setSize(300,300);
+
+        panelCenter.add(scrollPane, BorderLayout.CENTER);
+
+        shoppingCart.add("Center", panelCenter);
+        //------------------------------------------------//
+        JPanel panelBottom=new JPanel(new GridLayout(8,2));
+        int topPadding = 20;
+        int leftPadding = 15;
+        panelBottom.setBorder(BorderFactory.createEmptyBorder(30, 10, 10, 10));
+        JLabel selecetedTextLabel=new JLabel("Selected Product Details");
+        panelBottom.add(selecetedTextLabel);
+        shoppingCart.add("South",panelBottom);
+
+        shoppingCart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        shoppingCart.setVisible(true);
+    }
 
     public void addAProduct() {
         System.out.println("Adding a product");
